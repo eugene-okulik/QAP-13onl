cf24476a51ee9ad2978a75e8ef6cf13a1e1ca0cf
Eugene Okulik <tiburon854@gmail.com>
2023-03-31 19:44:07 +0300

    Homework 1 done - fixed lint

--- homework/eugene_okulik/homework_1/hello.py
+++ homework/eugene_okulik/homework_1/hello.py
@@ -1,2 +1,2 @@
-print('alsdkas flsdkfjalskdf sadflkasjdflkasdf saldkfjasldkfj sadlkfjsldkjf  sladkjflksdajf lksadjflksdjl laskdjflasdkjflsadkj lkajsdlkasdfj')
+print('alsdkas flsdkfjalskdf sadflkasjdflkasdf saldkfjasldkfj sadlkfjsldkjf')
 print('Hello world')
