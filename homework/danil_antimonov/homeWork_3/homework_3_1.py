ed524e72ea9d4dfdb0caaea8b3688124b42e5698
slnjke <antimonov342@mail.ru>
2023-04-02 00:24:14 +0400

    Homework3_done

--- homework/danil_antimonov/homeWork_3/homework_3_1.py
+++ homework/danil_antimonov/homeWork_3/homework_3_1.py
@@ -6,5 +6,5 @@ summ = a + b
 print(f'Сумма {a} и {b} равна {summ}')
 diff = a - b
 print(f'Разность {a} и {b} равна {diff}')
-multiplication = a * b
+multiplication = b * a
 print(f'Произведение {a} и {b} равно {multiplication}')
