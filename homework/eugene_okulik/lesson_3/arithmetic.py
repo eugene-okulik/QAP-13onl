6ace14654a248f4dfa0f6509d3e14f65f3389a8e
Eugene Okulik <tiburon854@gmail.com>
2023-04-01 10:20:56 +0300

    Files from lesson 3

---
+++ homework/eugene_okulik/lesson_3/arithmetic.py
@@ -0,0 +1,16 @@
+print(13 + 5)
+sum_nums = 15 + 6
+print(sum_nums)
+a = 3
+b = 5
+sum_nums2 = a + b
+print(sum_nums2)
+print(2 * 3)
+print(2 - 3)
+print(2 / 3)
+print(10 % 3)
+print(20 % 2)
+print(5 ** 2)
+print(7 ** 3)
+print(20 / 11)
+print(20 // 11)
---
+++ homework/eugene_okulik/lesson_3/input_types.py
@@ -0,0 +1,7 @@
+user_num = input('enter some number: ')
+user_num = float(user_num)
+phrase1 = 'You entered '
+joined_phrase = phrase1 + str(user_num)
+
+
+print(joined_phrase, 'your number + 5 =', user_num + 5)
