1735b2405b54871f536fdf67f66ded1b23437fe8
slnjke <antimonov342@mail.ru>
2023-04-02 00:10:11 +0400

    Files for homework3 is added

---
+++ homework/danil_antimonov/homeWork_3/homework_3_1.py
@@ -0,0 +1,10 @@
+# Даны 2 действительных числа a и b. Получить их сумму, разность и произведение
+
+a = 3.4
+b = 5
+summ = a + b
+print(f'Сумма {a} и {b} равна {summ}')
+diff = a - b
+print(f'Разность {a} и {b} равна {diff}')
+multiplication = a * b
+print(f'Произведение {a} и {b} равно {multiplication}')
