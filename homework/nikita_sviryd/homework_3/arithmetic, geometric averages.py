996535b9e7c1937759c18c6ca6dd7889a9bd0546
Nikita Svirid <qa.sviryd@gmail.com>
2023-04-03 17:19:54 +0300

    completed homework number 3

---
+++ homework/nikita_sviryd/homework_3/arithmetic, geometric averages.py
@@ -0,0 +1,9 @@
+a, b = int(input()), int(input())
+
+geometric_mean = (a * b) ** 0.5
+
+print(geometric_mean)
+
+arithmetic_mean = (a + b) / 2
+
+print(arithmetic_mean)
---
+++ homework/nikita_sviryd/homework_3/triangle.py
@@ -0,0 +1,6 @@
+a, b = int(input()), int(input())
+
+hypotenuse = (a**2 + b**2) * 0.5
+area = 0.5 * a * b
+
+print(hypotenuse, area, sep='\n')
