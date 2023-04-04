e212f721ed17209d627f4139cd48130362f9ded8
Viktor_Nesterov <nesteroff897@gmail.com>
2023-04-04 10:50:03 +0300

    Home_work_3

---
+++ homework/nesterov_viktor/Home_work_3/task3.py
@@ -0,0 +1,8 @@
+a = 64
+b = 24
+
+arithmetic_mean = (a + b) / 2
+geometric_mean = (a * b) ** 0.5
+
+print("Среднее арифметическое чисел a и b:", arithmetic_mean)
+print("Среднее геометрическое чисел a и b:", geometric_mean)
---
+++ homework/nesterov_viktor/Home_work_3/task5.py
@@ -0,0 +1,7 @@
+side = int(input("Введите длину ребра куба: "))
+
+volume = side ** 3
+area = 6 * side ** 2
+
+print("Объем куба:", volume)
+print("Площадь боковой поверхности куба:", area)
