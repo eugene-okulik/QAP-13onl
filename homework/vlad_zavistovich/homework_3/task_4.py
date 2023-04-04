2d2c58d360950df2202f3241d2979c3893dac2b0
VladZera <vladzevar@gmail.com>
2023-04-04 14:27:21 +0200

    added the solutions for the homework tasks 1-5

---
+++ homework/vlad_zavistovich/homework_3/task_4.py
@@ -0,0 +1,6 @@
+katet_1 = 4
+katet_2 = 6
+gepotenuza = (katet_1 ** 2 + katet_2 ** 2) ** 0.5
+print('Длина гепотенузы равна:', gepotenuza)
+s = (katet_1 * katet_2) / 2
+print('Площадь прямоугольного треугольника равна:', s)
