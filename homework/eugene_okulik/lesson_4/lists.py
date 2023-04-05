1a331c600da38cea117d3f6d184c2ab3f0a01f5b
Eugene Okulik <tiburon854@gmail.com>
2023-04-05 08:45:40 +0300

    files from lesson 4

---
+++ homework/eugene_okulik/lesson_4/lists.py
@@ -0,0 +1,15 @@
+my_list = [1, 3, 6, 7, 'text', False, 12.3, [4, 5]]
+my_list1 = list()
+my_list2 = []
+first_elt = my_list[0]
+print(my_list[2])
+print(len(my_list))
+print([4, 5] in my_list)
+print(my_list)
+my_list.append(42)
+print(my_list)
+my_list[1] = 5
+print(my_list)
+x = my_list.pop(1)
+print(my_list, x)
+my_list3 = [42]
