6b7de09268c20f30da141fefbffb3a883f36af89
Anastasiya Shkredava <shkredavanasta@gmail.com>
2023-04-05 11:52:38 +0300

    whitespaces were added around arithmetic operator (lines 19, 20)

--- homework/anastasiya_shkredava/homework4/task.py
+++ homework/anastasiya_shkredava/homework4/task.py
@@ -16,7 +16,7 @@ my_dict['dict'].pop((1, 2))
 my_dict['set'].add(23)
 my_dict['set'].remove(1.234)
 print('The first 8 characters of the string:', my_dict['str'][:7])
-mid = len(my_dict['str'])//2
-print('4 characters from the middle of the string:', my_dict['str'][mid-2:mid+2])
+mid = len(my_dict['str']) // 2
+print('4 characters from the middle of the string:', my_dict['str'][mid - 2:mid + 2])
 print('Characters with indices that are multiples of 3:', my_dict['str'][3::3])
 pprint.pprint(my_dict, sort_dicts=False)
