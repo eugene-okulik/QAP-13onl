62f7d1d54295cc2cba02589cd761bf35f2c6a8e1
slnjke <antimonov342@mail.ru>
2023-04-07 20:34:11 +0400

    homework_4 progress save#2

--- homework/danil_antimonov/homeWork_4/homework_4_1.py
+++ homework/danil_antimonov/homeWork_4/homework_4_1.py
@@ -3,13 +3,13 @@
 
 my_dict = {'tuple': (1, 3, 6, 7, 'text', False, 12.3), 'list': [1, 2, 3, 4, 5],
            'dict': {'list': [213, 4345, 45645], 'name': 'George'},
-           'set': {'text1', 'text2', 1, 3, 6, 7, 'text', False, 12.3}, 'str': 'string'}
+           'set': {'text1', 'text2', 1, 3, 6, 7, 'text', False, 12.3}, 'str': 'Wery_long_string'}
 
 # Для того, что хранится под ключом ‘tuple’:
 # выведите на экран все элементы начиная со второго и до конца
 
-print(my_dict.values())
 tuple_in_my_dict = my_dict['tuple']
+print('Содержимое вложенного Tuple до изменений:\n', tuple_in_my_dict)
 print('Содержимое вложенного Tuple начиная со 2 элемента:\n', tuple_in_my_dict[1:])
 
 # Для того, что хранится под ключом ‘list’:
@@ -36,11 +36,21 @@ print('Вложенный словарь после изменений:\n', dict
 # добавьте новый элемент в множество
 # удалите элемент из множества
 
+set_in_my_dict = my_dict['set']
+print(set_in_my_dict)
+print(set_in_my_dict.add(324234))
+print(set_in_my_dict.pop())
 
 # Для того, что хранится под ключом ‘str’:
 # Выведите на экран из строки следующие срезы:
-#
 # первые восемь символов
 # четыре символа из центра строки
 # символы с индексами кратными трем
 # В конце выведите на экран весь словарь
+
+str_in_my_dict = my_dict['str']
+print('Первые восемь символов строки:\n', str_in_my_dict[:9])
+print('Длина строки:\n', len(str_in_my_dict))
+print('Четыре символа из центра строки:\n', str_in_my_dict[6:10])
+print('Символы с индексами кратными трем:\n', str_in_my_dict[::3])
+print('Изначальный словарь:\n', my_dict)
