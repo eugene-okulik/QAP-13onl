1a7ae9a9e4f8227eb4cf3a8e0f5a4c01e758d33d
AlinaEgorova <egorovalina29@mail.ru>
2023-04-08 05:14:46 +1000

    homework_3.4 done

---
+++ homework/alina_egorova/homework3/4.py
@@ -0,0 +1,8 @@
+from math import sqrt
+
+a = 3
+b = 4
+hypotenuse = sqrt(a ** 2 + b ** 2)
+square = (a * b) / 2
+print('Гипотенуза прямоугольного треугольника равна ', hypotenuse)
+print('Площадь прямоугольного треугольника равна ', square)
