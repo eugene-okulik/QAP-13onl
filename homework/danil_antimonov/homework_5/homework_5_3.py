b95ebe9159bf5d177e7305ce8c308a2b9b13a73b
slnjke <antimonov342@mail.ru>
2023-04-10 11:53:20 +0400

    cosmetic changes

--- homework/danil_antimonov/homework_5/homework_5_3.py
+++ homework/danil_antimonov/homework_5/homework_5_3.py
@@ -9,7 +9,7 @@ while i < 15:
     my_list.append(round(random.random() * 10))
     i += 1
 print(my_list)
-print(max(my_list))
-print(min(my_list))
-print(sum(my_list))
-print(sum(my_list) / i)
+print('Максимальное сгенерённое число:\n', max(my_list))
+print('Минимальное сгенерённое число:\n', min(my_list))
+print('Сумма всех сгенерённых чисел:\n', sum(my_list))
+print('Среднее арифметическое:\n', sum(my_list) / i)
