b79e47451f0bd3edbc9d1a7b3edd1a807e63decb
VladZera <vladzevar@gmail.com>
2023-04-12 10:08:40 +0200

    Added my tasks 1-3 homework_5

---
+++ homework/vlad_zavistovich/homework_5/task_1.py
@@ -0,0 +1,16 @@
+text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. ' \
+          'Integer urna nisl, ' \
+          'facilisis vitae semper at, dignissim vitae libero'
+my_list = text.split()
+print(my_list)
+
+for word in my_list:
+    if ',' in word:
+        word = word.replace(',', 'ing,')
+        print(word, end=' ')
+    elif '.' in word:
+        word = word.replace('.', 'ing.')
+        print(word, end=' ')
+    else:
+        word = f'{word}ing'
+        print(word, end=' ')
