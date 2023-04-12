075f488a14e83f710f7b44d28b70a9d40756b02c
VladZera <vladzevar@gmail.com>
2023-04-12 10:19:19 +0200

    fixed some bugs

--- homework/vlad_zavistovich/homework_5/Task_3.py
+++ homework/vlad_zavistovich/homework_5/Task_3.py
@@ -3,7 +3,7 @@ from random import randint
 
 my_list = [randint(0, 100) for i in range(15)]
 
-print('Список 15-ти рандомных чисел', my_list)  # В задании не было указано это распечатывать, но я вывел для проверки.
+print('Список 15-ти рандомных чисел', my_list)
 print('Максимальное число из рандомов =', max(my_list))
 print('Минимальное число из рандомов =', min(my_list))
 print('Сумма всех рандомов =', sum(my_list))
--- homework/vlad_zavistovich/homework_5/task_1.py
+++ homework/vlad_zavistovich/homework_5/task_1.py
@@ -1,6 +1,6 @@
 text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. ' \
-          'Integer urna nisl, ' \
-          'facilisis vitae semper at, dignissim vitae libero'
+       'Integer urna nisl, ' \
+       'facilisis vitae semper at, dignissim vitae libero'
 my_list = text.split()
 print(my_list)
 
