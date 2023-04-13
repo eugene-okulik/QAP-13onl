f1f983cb59e86531e7ecf542bdbc894363c01fc9
Vika Avrusevich <vika@gmail.com>
2023-04-13 15:17:53 +0300

    vika avrusevich: homework 5 completed (version 2)

---
+++ homework/vika_avrusevich/homework 5/task 1.py
@@ -0,0 +1,20 @@
+# Напишите программу, которая добавляет ‘ing’ к словам (к каждому слову)
+# в тексте “Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at,
+# dignissim vitae libero” и после этого выводит получившийся текст на экран.
+
+
+string = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. ' \
+         'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+new_list = string.split(' ')
+print(new_list)
+
+for word in new_list:
+    if ',' in word:
+        word = word.replace(',', 'ing,')
+        print(word, end=' ')
+    elif '.' in word:
+        word = word.replace('.', 'ing.')
+        print(word, end=' ')
+    else:
+        word = f'{word}ing'
+        print(word, end=' ')
