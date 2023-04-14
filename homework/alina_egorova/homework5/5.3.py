0c7c0c6e9b1894ebe78d02de14affa9256cac3f0
AlinaEgorova <egorovalina29@mail.ru>
2023-04-14 19:21:36 +1000

    homework 5.3 done

---
+++ homework/alina_egorova/homework5/5.3.py
@@ -0,0 +1,13 @@
+# Выполните random.random() 15 раз, сохраняя куда-то каждый результат.
+import random
+
+random_list = []
+
+for i in range(15):
+    random_num = random.random()
+    random_list.append(random_num)
+
+print(max(random_list))  # максимальное из сгенеренных рандомов
+print(min(random_list))  # минимальное из сгенеренных рандомов
+print(sum(random_list))  # сумма из сгенеренных рандомов
+print((sum(random_list)) / 15)  # среднее арифметическое
