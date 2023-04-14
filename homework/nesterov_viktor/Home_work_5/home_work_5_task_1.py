06b67f612ecc29eefc78ae60e08da26d2dcf40bb
Viktor_Nesterov <nesteroff897@gmail.com>
2023-04-14 21:39:50 +0300

    Home_work_5_repair

--- homework/nesterov_viktor/Home_work_5/home_work_5_task_1.py
+++ homework/nesterov_viktor/Home_work_5/home_work_5_task_1.py
@@ -1,4 +1,5 @@
-text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel." \
+       " Integer urna nisl,facilisis vitae semper at, dignissim vitae libero"
 
 # Разбиваем текст на отдельные слова
 words = text.split()
