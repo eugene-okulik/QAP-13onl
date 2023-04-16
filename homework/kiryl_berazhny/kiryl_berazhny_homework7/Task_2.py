42dcb81fb1ca0fe79a2aaf416d693c8d5cd2e1b0
Kiryl Berazhny <kirylberag@gmail.com>
2023-04-16 12:07:25 +0200

    Убрал лишний проблем

--- homework/kiryl_berazhny/kiryl_berazhny_homework7/Task_2.py
+++ homework/kiryl_berazhny/kiryl_berazhny_homework7/Task_2.py
@@ -3,9 +3,9 @@
 my_list = [int(num) for num in input('Enter numbers: ').split()]
 my_list = list(filter(lambda num: 0 <= num <= 19, my_list))[:4]  # сначала фильтрует по величине, затем по длине
 number_names = {
-                0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven',
-                8: 'eight', 9: 'nine', 10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen',
-                15: 'fifteen', 16: 'sixteen', 17: 'seventeen',  18: 'eighteen', 19: 'nineteen'
+        0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine',
+        10: 'ten', 11: 'eleven', 12: 'twelve',
+        13: 'thirteen', 14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'
 }
 new_dict = dict()
 for key, value in number_names.items():
