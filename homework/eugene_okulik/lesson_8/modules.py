3ecaf35984a41cafa86544e0761e2274bc6e5abf
Eugene Okulik <tiburon854@gmail.com>
2023-04-19 09:18:43 +0300

    files from lesson 8

--- homework/eugene_okulik/lesson_8/modules.py
+++ homework/eugene_okulik/lesson_8/modules.py
@@ -1,13 +1,11 @@
 # import import_me
-from import_me import *
-from homework.eugene_okulik.lesson_7.import7 import *
-from homework.eugene_okulik.lesson_7 import *
+from homework.eugene_okulik.lesson_7 import import7
 from import_me import prettify, my_var, hello
 from import_me import my_name_is_very_long_and_uncomfortable as short_name
 from random import randint
 import random
 print(my_var)
-print(my_var2)
+print(import7.my_var2)
 
 
 hello()
