fa00d58c1729bf387abcd9e03d20e1ed5419769a
Kiryl Berazhny <kirylberag@gmail.com>
2023-04-19 12:00:44 +0200

    Перенес скобку

--- homework/kiryl_berazhny/kiryl_berazhny_homework7/Task_2.py
+++ homework/kiryl_berazhny/kiryl_berazhny_homework7/Task_2.py
@@ -5,7 +5,8 @@ my_list = list(filter(lambda num: 0 <= num <= 19, my_list))[:100]  # снача�
 number_names = {
     0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine',
     10: 'ten', 11: 'eleven', 12: 'twelve',
-    13: 'thirteen', 14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'}
+    13: 'thirteen', 14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'
+}
 new_dict = dict()
 for key, value in number_names.items():
     new_dict[value] = key
