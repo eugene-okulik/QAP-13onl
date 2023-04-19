33f10953863f8ad52c9a71563c5773ca14ee0a0c
Pavel Laushuk <sn0oKkZ1F@yandex.ru>
2023-04-20 00:14:50 +0300

    fix whitespace , homework done

--- homework/pavel_lavshuk/homework_5/task_1.py
+++ homework/pavel_lavshuk/homework_5/task_1.py
@@ -1,16 +1,8 @@
 my_text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper '
            'at, dignissim vitae libero')
 
-my_text3 = my_text.split()
-print(my_text3)
-for word in my_text3:
-    if word[-1] in [',', '.']:
-        x = word[-1]
-        my_text = ' '.join(my_text3)
-        my_text2 = my_text.replace(',', '')
-        my_text1 = my_text2.replace('.', '')
-        my_text4 = my_text1.split()
-        new_text = ' '.join(f'{word}ing' for word in my_text4)
-        new_text2 = new_text.split()
-# Так и не понял как добавить знаки пунктуации именно к тем словам в которых они были,если конечно все что выше
-# правильно, хотелось бы подсказки.
+my_text1 = my_text.split()
+new_text = ' '.join(f'{word}ing' for word in my_text1)
+new_text1 = new_text.replace(',ing', 'ing,')
+new_text2 = new_text1.replace('.ing', 'ing.')
+print(new_text2)
