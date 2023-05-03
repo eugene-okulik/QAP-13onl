08f1d5b00b86bdf6a7b56f99a3bbe36b813792d5
Eugene Okulik <tiburon854@gmail.com>
2023-05-03 09:47:43 +0300

    files from lesson 11

---
+++ homework/eugene_okulik/lesson11/country_data.py
@@ -0,0 +1,16 @@
+import json
+
+
+class CountryData:
+    def __init__(self, my_filename):
+        self.filename = my_filename
+        self.data = json.loads(self.read_file())
+        self.country = self.data['Country']
+        self.avg_temp = self.data['avg_temp']
+
+    def read_file(self):
+        with open(self.filename, 'r') as data_file:
+            return data_file.read()
+        
+
+var = 1
