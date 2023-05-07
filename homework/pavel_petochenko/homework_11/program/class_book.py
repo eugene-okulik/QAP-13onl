3ae76840790273bf3db111a8156eac30a14b4180
Pavel Petochenko <p.petochenko@gmail.com>
2023-05-07 19:51:24 +0300

    Homework 11 done.

---
+++ homework/pavel_petochenko/homework_11/program/class_book.py
@@ -0,0 +1,10 @@
+# Для этого файла:
+# Класс должен содержать информацию о книге - класс Book.
+# Атрибуты: количество страниц, год издания, автор, цена.
+
+class Book:
+    def __init__(self, num_page, year, author, price):
+        self.num_page = num_page
+        self.year = year
+        self. author = author
+        self.price = price
