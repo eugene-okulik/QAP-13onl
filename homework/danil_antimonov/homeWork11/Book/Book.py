5ab2d59b65bedebd9c7d711754673215942bc557
slnjke <antimonov342@mail.ru>
2023-05-12 20:07:31 +0400

    fix flake8

--- homework/danil_antimonov/homeWork11/Book/Book.py
+++ homework/danil_antimonov/homeWork11/Book/Book.py
@@ -1,5 +1,5 @@
 class Book:
-    def __init__(self, name, pages,  year_of_publication, author, price):
+    def __init__(self, name, pages, year_of_publication, author, price):
         self.name = name
         self.pages = pages
         self.year_of_publication = year_of_publication
