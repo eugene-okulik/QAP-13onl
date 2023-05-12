2d8924ff032087b51d9be706ad81003f0ad84c4d
slnjke <antimonov342@mail.ru>
2023-05-12 20:05:08 +0400

    Homework_11 files added

---
+++ homework/danil_antimonov/homeWork11/Book/Book.py
@@ -0,0 +1,7 @@
+class Book:
+    def __init__(self, name, pages,  year_of_publication, author, price):
+        self.name = name
+        self.pages = pages
+        self.year_of_publication = year_of_publication
+        self.author = author
+        self.price = price
