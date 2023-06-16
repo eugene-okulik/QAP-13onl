4f6a27be9f4db01932cab0ff9cea160dc8fc1de3
Anastasiya Shkredava <shkredavanasta@gmail.com>
2023-06-16 12:01:35 +0300

    homework 21 done

---
+++ homework/anastasiya_shkredava/homework21/test_hw21.py
@@ -0,0 +1,55 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.alert import Alert
+
+
+@pytest.fixture()
+def driver():
+    chrome_driver = webdriver.Chrome()
+    chrome_driver.maximize_window()
+    return chrome_driver
+
+
+def test_one(driver):
+    driver.get('https://www.demoblaze.com/index.html')
+    driver.execute_script("window.scrollTo(0, 900)")
+    WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'card-block')))
+    items = driver.find_elements(By.CLASS_NAME, "card-title")
+    item_to_add = items[0].text
+    actions = ActionChains(driver)
+    actions.key_down(Keys.CONTROL).click(items[0]).key_up(Keys.CONTROL).perform()
+    driver.switch_to.window(driver.window_handles[1])
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[onclick='addToCart(1)']")))
+    driver.find_element(By.CSS_SELECTOR, "[onclick='addToCart(1)']").click()
+    WebDriverWait(driver, 10).until(EC.alert_is_present())
+    Alert(driver).accept()
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
+    driver.find_element(By.ID, "cartur").click()
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='tbodyid']/tr/td[2]")))
+    added_item = driver.find_element(By.XPATH, "//*[@id='tbodyid']/tr/td[2]")
+    assert item_to_add == added_item.text
+
+
+def test_two(driver):
+    driver.get('https://demoqa.com/menu#')
+    driver.execute_script("window.scrollTo(0, 300)")
+    actions = ActionChains(driver)
+    actions.move_to_element(driver.find_element(By.LINK_TEXT, 'Main Item 2'))
+    actions.move_to_element(driver.find_element(By.XPATH, '//*[@id="nav"]/li[2]/ul/li[3]/a'))
+    actions.move_to_element(driver.find_element(By.XPATH, '//*[@id="nav"]/li[2]/ul/li[3]/ul/li[2]/a'))
+    actions.perform()
+
+
+def test_three(driver):
+    driver.get('https://www.qa-practice.com/elements/alert/prompt')
+    driver.find_element(By.LINK_TEXT, 'Click').click()
+    Alert(driver).send_keys('some text')
+    Alert(driver).accept()
+    result_text = driver.find_element(By.ID, 'result-text').text
+    assert result_text == 'some text'
