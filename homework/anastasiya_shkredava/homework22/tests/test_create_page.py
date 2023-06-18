bfc882d1855642ea8e704e07656cf4d7ba66b643
Anastasiya Shkredava <shkredavanasta@gmail.com>
2023-06-18 18:04:41 +0300

    homework 22 done

--- homework/anastasiya_shkredava/homework22/tests/test_create_page.py
+++ homework/anastasiya_shkredava/homework22/tests/test_create_page.py
@@ -10,7 +10,7 @@ def test_confirm_password(driver):
     create_page.enter_password("123456aA!@")
     create_page.confirm_password("123456aA!#")
     create_page.click_create_an_account_button()
-    assert create_page.confirmation_error.text == 'Please enter the same value again.'
+    assert create_page.confirmation_error.is_displayed()
 
 
 def test_short_password(driver):
@@ -22,8 +22,7 @@ def test_short_password(driver):
     create_page.enter_password("1234567")
     create_page.confirm_password("1234567")
     create_page.click_create_an_account_button()
-    assert create_page.password_error.text == 'Minimum length of this field must be equal or greater than 8 symbols. ' \
-                                              'Leading and trailing spaces will be ignored.'
+    assert create_page.password_error.is_displayed()
 
 
 def test_missing_required_field(driver):
@@ -34,4 +33,4 @@ def test_missing_required_field(driver):
     create_page.enter_password("123456aA!@")
     create_page.confirm_password("123456aA!@")
     create_page.click_create_an_account_button()
-    assert create_page.last_name_error.text == 'This is a required field.'
+    assert create_page.last_name_error.is_displayed()
