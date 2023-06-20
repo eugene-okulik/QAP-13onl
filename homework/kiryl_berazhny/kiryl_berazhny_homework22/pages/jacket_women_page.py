e5cd43d853f58e364c8feaf64ccc00126eeefdf6
Kiryl Berazhny <kirylberag@gmail.com>
2023-06-20 17:52:41 +0200

    Исправил тест test_compare_check: товар не всегда не успевал добавиться в "сравнение" до перехода на другую страницу

--- homework/kiryl_berazhny/kiryl_berazhny_homework22/pages/jacket_women_page.py
+++ homework/kiryl_berazhny/kiryl_berazhny_homework22/pages/jacket_women_page.py
@@ -1,6 +1,9 @@
 from pages.base_page import BasePage
 from pages.locators import jackets_women_locators as JWL
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
 class JacketWomenPage(BasePage):
@@ -15,3 +18,7 @@ class JacketWomenPage(BasePage):
             move_to_element(self.finds(JWL.add_to_compare)[0]).\
             click().\
             perform()
+        WebDriverWait(self.driver, 10).until(
+            EC.text_to_be_present_in_element(
+                (By.XPATH, '/html/body/div[1]/header/div[2]/ul/li/a'), 'Compare Products 1 item')
+        )
--- homework/kiryl_berazhny/kiryl_berazhny_homework22/pages/locators/jackets_women_locators.py
+++ homework/kiryl_berazhny/kiryl_berazhny_homework22/pages/locators/jackets_women_locators.py
@@ -4,3 +4,4 @@ from selenium.webdriver.common.by import By
 all_products = (By.CLASS_NAME, 'product-item-info')
 add_to_compare = (By.CLASS_NAME, 'tocompare')
 button_sale = (By.ID, 'ui-id-8')
+check_text = (By.XPATH, '/html/body/div[1]/header/div[2]/ul/li/a')
